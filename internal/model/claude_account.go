87f383f78cec192f5c0a4d8dc97f55db701f00bd
Lemon <lemon@linux.do>
2024-10-25 18:49:00 +0800

    refactor: 移除无用代码和过期注释

--- internal/model/claude_account.go
+++ internal/model/claude_account.go
@@ -12,7 +12,6 @@ type ClaudeAccount struct {
 	Status     int       `json:"status" gorm:"not null;default:1" comment:"状态, 1:正常, 0:禁用" column:"status"`
 	CreateTime time.Time `json:"createTime" gorm:"not null" comment:"创建时间" column:"create_time"`
 	UpdateTime time.Time `json:"updateTime" gorm:"not null" comment:"更新时间" column:"update_time"`
-	// ExpireAt   time.Time `json:"expireAt" gorm:"not null" comment:"过期时间" column:"expire_at"`
 }
 
 func (m *ClaudeAccount) TableName() string {
--- internal/server/migration.go
+++ internal/server/migration.go
@@ -30,37 +30,10 @@ func (m *Migrate) Start(ctx context.Context) error {
 		m.log.Error("user migrate error", zap.Error(err))
 		return err
 	}
-	// TODO 待处理
-	// m.initClaudeAccount()
 	m.log.Info("AutoMigrate success")
 	return nil
 }
 func (m *Migrate) Stop(ctx context.Context) error {
 	m.log.Info("AutoMigrate stop")
 	return nil
-}
-
-func (m *Migrate) initClaudeAccount() {
-	var accounts []*model.ClaudeAccount
-	// 查出ClaudeAccount 过期时间为空的数据
-	m.db.Where("expire_at is null").Find(&accounts)
-	// 判断是否有数据
-	if len(accounts) == 0 {
-		return
-	}
-
-	// 遍历数据，处理
-	for _, account := range accounts {
-		userId := account.UserId
-		// 查询关联的用户
-		var user model.User
-		m.db.Where("id = ?", userId).First(&user)
-		// 判断用户是否存在
-		if user.ID == 0 {
-			continue
-		}
-		// 更新过期时间
-		// account.ExpireAt = user.ExpirationTime
-		m.db.Save(&account)
-	}
 }
